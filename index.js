b35a35ca7c1e2ecaebb8a3c8be6032d8ea1e92bb
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-17 17:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var namer = "Susan"
+var name = "Joe"
 var height = "74"
 
-if (namer === "Susan") {
+if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log("wrong name man")
 }
 // Don't worry about this
-module.exports = { namer, height
+module.exports = { name, height
 }
 
