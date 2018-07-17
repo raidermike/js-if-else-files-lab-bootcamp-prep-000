71f5249d5099e1afa0789ba9cafcaed60bed33bc
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-17 17:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("The name is correct")
+} else {
+  console.log("wrong name man")
 }
 // Don't worry about this
 module.exports = { name, height
